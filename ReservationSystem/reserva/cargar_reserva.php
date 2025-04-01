89382f5ade8a524cdc46267bd02022c0a90ed9fd
Bernard2806 <berramuspegonzalez@gmail.com>
2025-04-01 10:29:12 -0300

    feat: Reemplazar botón de anulación por enlace y simplificar lógica de reserva

--- ReservationSystem/reserva/cargar_reserva.php
+++ ReservationSystem/reserva/cargar_reserva.php
@@ -335,7 +335,7 @@ $esAdmin = isset($_SESSION['EsAdmin']) && $_SESSION['EsAdmin'] == true;
                         </div>
 
                         <div class="mt-8 space-y-4">
-                            <button type="submit" name="boton" value="1"
+                            <button type="submit"
                                 class="w-full flex justify-center items-center px-5 py-3 bg-gradient-to-r from-green-600 to-green-700 text-white rounded-md font-medium hover:from-green-700 hover:to-green-800 transition-all focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-dark-850">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
@@ -343,13 +343,13 @@ $esAdmin = isset($_SESSION['EsAdmin']) && $_SESSION['EsAdmin'] == true;
                                 Confirmar Reserva
                             </button>
 
-                            <button type="submit" name="boton" value="0"
+                            <a href="../index.php"
                                 class="w-full flex justify-center items-center px-5 py-3 bg-gray-700 text-white rounded-md font-medium hover:bg-gray-600 transition-all focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-dark-850">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                                 Anular Reserva
-                            </button>
+                            </a>
 
                             <div class="flex justify-start">
                                 <button type="button" @click="currentStep = 2"
