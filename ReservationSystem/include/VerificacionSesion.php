23e4fc805a1dbde6de9c11fd032499d5c6a46981
Bernardo <berramuspegonzalez@gmail.com>
2026-01-21 19:55:12 -0300

    feat: Añadir .htaccess para configurar la reescritura de URLs y dirigir las solicitudes al directorio public; eliminar todo el código legacy previo al refactor hacia arquitectura MVC

--- ReservationSystem/include/VerificacionSesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['loggedIn'])) {
-    echo "<script>alert('No has iniciado sesión.'); window.location.href = 'index.php';</script>";
-    exit();
-}
-?>
--- ReservationSystem/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require 'include/VerificacionSesion.php';
-    session_start();
-    $_SESSION = array();
-    session_destroy();
-    header("location: index.php");
-    exit();
-?>
