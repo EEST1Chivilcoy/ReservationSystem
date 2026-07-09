71d4b7de4ddcbef34110534bf3490f81cd409d9a
Bernardo <berramuspegonzalez@gmail.com>
2026-07-09 12:50:13 -0300

    fix: mostrar reservas canceladas aunque no haya reservas próximas ni pasadas

--- ReservationSystem/mis_reservas.php
+++ ReservationSystem/mis_reservas.php
@@ -223,7 +223,7 @@ foreach ($reservas as $reserva) {
             </a>
         </section>
 
-        <?php if (empty($reservas_proximas) && empty($reservas_pasadas)): ?>
+        <?php if (empty($reservas_proximas) && empty($reservas_pasadas) && empty($reservas_canceladas)): ?>
             <div class="bg-gray-800 rounded-xl p-8 text-center border border-gray-700 shadow-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mx-auto text-gray-500 mb-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
